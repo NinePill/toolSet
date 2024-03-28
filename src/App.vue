94b80be6e00ca8eeab531262a37e576ac09eb726
if <fukai_sun@kingdee.com>
2024-03-28 15:47:39 +0800

    功能完善

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import Sms from "@/components/Sms.vue";
 import Translation from "@/components/Translation.vue";
 import Sign from "@/components/Sign.vue";
 import Ocr from "@/components/Ocr.vue";
+import { Check, Close } from '@element-plus/icons-vue'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
@@ -32,6 +33,12 @@ const menulList = ref(
       {id: 1009, "name": "ocr识别"},
     ]
 )
+const coffee = ref(false)
+const activePay = ref('wechat')
+const selectPay = (pay) => {
+  activePay.value = pay;
+}
+
 </script>
 
 <template>
@@ -60,7 +67,24 @@ const menulList = ref(
             <el-switch
                 v-model="useSetting.darkTheme"
                 @change="toggleDark"
+                inline-prompt
+                active-text="深色"
+                inactive-text="浅色"
             />
+            <el-tooltip
+                class="box-item"
+                effect="dark"
+                content="打赏"
+                placement="bottom"
+            >
+            <el-button circle class="coffee-btn" @click="coffee=true"><svg width="28" height="28" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg"
+                     class="video-coin-icon video-toolbar-item-icon">
+                  <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M14.045 25.5454C7.69377 25.5454 2.54504 20.3967 2.54504 14.0454C2.54504 7.69413 7.69377 2.54541 14.045 2.54541C20.3963 2.54541 25.545 7.69413 25.545 14.0454C25.545 17.0954 24.3334 20.0205 22.1768 22.1771C20.0201 24.3338 17.095 25.5454 14.045 25.5454ZM9.66202 6.81624H18.2761C18.825 6.81624 19.27 7.22183 19.27 7.72216C19.27 8.22248 18.825 8.62807 18.2761 8.62807H14.95V10.2903C17.989 10.4444 20.3766 12.9487 20.3855 15.9916V17.1995C20.3854 17.6997 19.9799 18.1052 19.4796 18.1052C18.9793 18.1052 18.5738 17.6997 18.5737 17.1995V15.9916C18.5667 13.9478 16.9882 12.2535 14.95 12.1022V20.5574C14.95 21.0577 14.5444 21.4633 14.0441 21.4633C13.5437 21.4633 13.1382 21.0577 13.1382 20.5574V12.1022C11.1 12.2535 9.52148 13.9478 9.51448 15.9916V17.1995C9.5144 17.6997 9.10883 18.1052 8.60856 18.1052C8.1083 18.1052 7.70273 17.6997 7.70265 17.1995V15.9916C7.71158 12.9487 10.0992 10.4444 13.1382 10.2903V8.62807H9.66202C9.11309 8.62807 8.66809 8.22248 8.66809 7.72216C8.66809 7.22183 9.11309 6.81624 9.66202 6.81624Z"
+                        fill="currentColor" data-darkreader-inline-fill=""
+                        style="--darkreader-inline-fill: currentColor;"></path>
+                </svg>
+              </el-button></el-tooltip>
           </el-col>
         </el-row>
 
@@ -79,10 +103,49 @@ const menulList = ref(
       <el-footer>版权所有</el-footer>
     </el-container>
   </el-container>
+
+
+  <el-dialog v-model="coffee" title="打赏作者" class="coffee-class" :modal="false" :width="600" center>
+    <div class="ac">
+      <p> 如果对您有价值，不妨支持一下我吧</p>
+      <p> 我将专注于持续优化产品体验。 </p>
+    </div>
+
+    <div class="ac mt20">
+      <el-button
+          :style="{ backgroundColor: activePay === 'wechat' ? '#27db70' : 'transparent',color:activePay === 'wechat' ? '#fff' : '#000' }"
+          @click="selectPay('wechat')">微信
+      </el-button>
+      <el-button
+          :style="{ backgroundColor: activePay === 'alipay' ? '#006ee8' : 'transparent',color:activePay === 'alipay' ? '#fff' : '#000' }"
+          @click="selectPay('alipay')">支付宝
+      </el-button>
+    </div>
+    <div class="ac mt20">
+      <img :src="activePay === 'wechat' ? '/paylogo/we-pay.jpg' : '/paylogo/ali-pay.jpg'" class="pay-logo">
+    </div>
+  </el-dialog>
 </template>
 
 
 <style scoped>
+.coffee-class {
+  margin-top: 50px;
+}
+.ac {
+  text-align: center;
+}
+
+.mt20 {
+  margin-top: 20px;
+}
+.pay-logo {
+  width: 300px; /* 设置图片宽度 */
+  height: 300px; /* 设置图片高度 */
+}
+.coffee-btn {
+  margin-left: 20px;
+}
 ::-webkit-scrollbar {
   color: #ececec;
   width: 5px;
--- src/components/Ocr.vue
+++ src/components/Ocr.vue
@@ -1,33 +1,6 @@
-<template>
-  <div class="input-and-output">
-    <div>
-       <div v-html="inputImg" class="input" contenteditable="true" id="image-container" @paste="handlePaste"></div>
-    </div>
-
-
-
-      <el-input
-        v-model="outPutOcr"
-        style="width: 700px"
-        :rows="30"
-        type="textarea"
-        resize="none"
-        placeholder="识别结果"
-        class="outPut"
-      />
-
-  </div>
-
-  <div class="input-and-output-option">
-    <el-button type="primary" class="del-class" @click="clearAll">清空所有</el-button>
-    <el-button type="success" class="copy-class" @click="copyToClipboard(outPutOcr)">复制结果</el-button>
-  </div>
-</template>
-
 <script setup>
 import {nextTick, ref} from 'vue';
 import {ElNotification} from 'element-plus';
-import { UploadFilled } from '@element-plus/icons-vue'
 
 // API endpoint for OCR
 const API_ENDPOINT = import.meta.env.VITE_CENTER_API + '/tool/o';
@@ -38,73 +11,70 @@ const debounceTimer = ref('')
 const inPutOcr = ref('')
 const outPutOcr = ref('')
 const handlePaste = (event) => {
-    const items = (event.clipboardData || event.originalEvent.clipboardData).items;
-    for (const item of items) {
-        if (item.type.indexOf('image') === 0) {
-            const blob = item.getAsFile();
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                inputImg.value = `<img src="${e.target.result}" alt="">`
-                nextTick(() => {
-                    handleDebouncedChange(inputImg.value);
-                });
-            };
-            reader.readAsDataURL(blob);
-        }
+  const items = (event.clipboardData || event.originalEvent.clipboardData).items;
+  for (const item of items) {
+    if (item.type.indexOf('image') === 0) {
+      const blob = item.getAsFile();
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        inputImg.value = `<img src="${e.target.result}" alt="">`
+        nextTick(() => {
+          handleDebouncedChange(inputImg.value);
+        });
+      };
+      reader.readAsDataURL(blob);
     }
+  }
 }
 
 function ocrTransform(input) {
-    return new Promise((resolve, reject) => {
-        let inputText;
-        const imgRegex = /<img.*?src=["'](.*?)["'].*?>/i;
-        const match = imgRegex.exec(input);
-        if (match && match[1]) {
-          inputText = match[1];
-        }
-        if (!inputText) {
-            return
-        }
-        fetch(API_ENDPOINT, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({image: inputText})
+  return new Promise((resolve, reject) => {
+    let inputText;
+    const imgRegex = /<img.*?src=["'](.*?)["'].*?>/i;
+    const match = imgRegex.exec(input);
+    if (match && match[1]) {
+      inputText = match[1];
+    }
+    if (!inputText) {
+      return
+    }
+    fetch(API_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({image: inputText})
+    })
+        .then(response => {
+          if (response.ok) {
+            return response.json(); // 将response对象转换为JSON格式
+          } else {
+            throw new Error('Network response was not ok.');
+          }
         })
-            .then(response => {
-                if (response.ok) {
-                    return response.json(); // 将response对象转换为JSON格式
-                } else {
-                    throw new Error('Network response was not ok.');
-                }
-            })
-            .then(data => {
-                console.log(data); // 输出JSON格式的数据
-                resolve(data.output) // 将返回的数据放入output元素中
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-    });
+        .then(data => {
+          console.log(data); // 输出JSON格式的数据
+          resolve(data.output) // 将返回的数据放入output元素中
+        })
+        .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+        });
+  });
 }
 
-const handleDebouncedChange =(newValue) => {
-      clearTimeout(debounceTimer.value);
-      debounceTimer.value = setTimeout(async () => {
-          try {
-            outPutOcr.value = await ocrTransform(newValue);
+const handleDebouncedChange = (newValue) => {
+  clearTimeout(debounceTimer.value);
+  debounceTimer.value = setTimeout(async () => {
+    try {
+      outPutOcr.value = await ocrTransform(newValue);
 
 
-          } catch (error) {
-              console.error('执行失败:', error);
-          }
-      }, 100);
-  }
-
-
-
+    } catch (error) {
+      console.error('执行失败:', error);
+    }
+  }, 100);
+}
 
 
 // Clear all input and output fields
@@ -116,56 +86,75 @@ function clearAll() {
 // Copy to clipboard
 function copyToClipboard(text) {
   navigator.clipboard.writeText(text)
-    .then(() => {
-      ElNotification({
-        title: 'Success',
-        message: '结果已复制到剪贴板',
-        type: 'success',
-        duration: 1000,
+      .then(() => {
+        ElNotification({
+          title: 'Success',
+          message: '结果已复制到剪贴板',
+          type: 'success',
+          duration: 1000,
+        });
+      })
+      .catch((error) => {
+        console.error('无法访问剪贴板:', error);
       });
-    })
-    .catch((error) => {
-      console.error('无法访问剪贴板:', error);
-    });
 }
-
 
 
 </script>
 
+<template>
+  <div class="input-and-output">
+    <div>
+      <div v-html="inputImg" class="input" contenteditable="true" id="image-container" @paste="handlePaste"></div>
+    </div>
+
+
+    <el-input
+        v-model="outPutOcr"
+        style="width: 700px"
+        :rows="30"
+        type="textarea"
+        resize="none"
+        placeholder="识别结果"
+        class="outPut"
+    />
+
+  </div>
+
+  <div class="input-and-output-option">
+    <el-button type="primary" class="del-class" @click="clearAll">清空所有</el-button>
+    <el-button type="success" class="copy-class" @click="copyToClipboard(outPutOcr)">复制结果</el-button>
+  </div>
+</template>
+
+
+
 <style scoped>
-.input-and-output{
+.input-and-output {
   display: flex;
   justify-content: center;
 }
-.input {
-    width: 700px;
-    height: 300px;
-    border: 1px solid #409eff;
-    outline: none;
-    font: 14px 微软雅黑;
-    padding: 10px;
-    box-sizing: border-box;
-    resize: none;
-    border-radius: 4px;
-    margin-right: 20px;
-    overflow: hidden;
 
+.input {
+  width: 700px;
+  height: 300px;
+  border: 1px solid #409eff;
+  outline: none;
+  font: 14px 微软雅黑;
+  padding: 10px;
+  box-sizing: border-box;
+  resize: none;
+  border-radius: 4px;
+  margin-right: 20px;
+  overflow: hidden;
 
 
 }
 
 .input img {
-  width: 100%!important; /* 设置图片宽度为父容器的宽度 */
+  width: 100% !important; /* 设置图片宽度为父容器的宽度 */
   height: auto; /* 保持图片的宽高比 */
 }
-
-
-
-
-
-
-
 
 
 .outPut {
